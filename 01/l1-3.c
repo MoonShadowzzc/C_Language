fe8d562602c582426f6e0497eddb1495b2d63476
unknown <rexxazzc@126.com>
2019-01-05 20:42:25 +0800

    例1-3

---
+++ 01/l1-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* 输出两行句子“Let's study the C Language.”和
+               “It's interesting.” */
+
+int main(void) 
+{
+	printf("Let's study the C Language.\n");
+	printf("It's interesting.\n");
+	return 0;
+}
