d422400dc1e4392133d0c619602907ae70d87b34
unknown <rexxazzc@126.com>
2019-01-06 21:43:26 +0800

    l1-17  用十进制、八进制、十六进制表示一个整形常量，按不同形式输出

---
+++ 01/l1-17.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* 用十进制、八进制、十六进制表示一个整形常量，按不同形式输出  */
+void main()
+{
+	printf("%d--%o--%x\n",21,21,21);
+	printf("%d--%o--%x\n",035,035,035);
+	printf("%d--%o--%x\n",0x1a,0x1a,0x1a);
+}
