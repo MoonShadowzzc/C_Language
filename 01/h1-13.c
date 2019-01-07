efd52987a15c9d4bec64ecc0559efa60abea794a
unknown <rexxazzc@126.com>
2019-01-07 11:06:16 +0800

    homework

---
+++ 01/h1-13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* 编写输出以下图形的程序
+     @   
+   @@$@@
+     @   
+   **
+   ###
+   3333
+    
+ */
+void main()
+{
+	printf("  @\n@@$@@\n  @\n");
+	printf("\n");
+	printf("**\n###\n3333\n");
+	printf("\n");
+	printf("  \x1\n\x1\x1\x3\x1\x1\n  \x1\n");
+	printf("\n");	
+}
+  
