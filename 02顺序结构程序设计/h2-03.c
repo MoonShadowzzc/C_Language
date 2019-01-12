debdef52ffdbac0a8715ed68117d75735e37ba75
unknown <rexxazzc@126.com>
2019-01-13 00:13:06 +0800

    从键盘输入两个数字字符并分别存放在字符型变量a，b中，要求通过程序将这两个数字相加后输出

--- 02顺序结构程序设计/h2-03.c
+++ 02顺序结构程序设计/h2-03.c
@@ -6,11 +6,11 @@
 #include <stdio.h>
 void main()
 {
-	char a = '\0', b = '\0';
-	printf("Input a(Type of char):");
-	a = getchar();
-	printf("Input b(Type of char):");
-	b = getchar();
+	char a = '0';
+    char b = '0';
+	printf("Input a, b(Type of char):\n");
+	scanf("%c,%c",&a, &b);
 	int sum = (a-'0') +(b-'0');
+
 	printf("%c + %c = %d", a, b, sum);
 }
