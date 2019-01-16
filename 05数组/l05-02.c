ce697a2eb25f3be091b340279d5c098a34c8dee2
unknown <rexxazzc@126.com>
2019-01-16 11:41:29 +0800

    字符数组

---
+++ 05数组/l05-02.c
@@ -0,0 +1,17 @@
+/* ×Ö·ûÊý×é */
+#include <stdio.h>
+void main()
+{
+	int i = 0;
+	char ch[4];
+	ch[0] = 'G';
+	ch[1] = 'o';
+	ch[2] = 'o';
+	ch[3] = 'd';
+	for(i=0; i<4; i++)
+	{
+		putchar(ch[i]);
+	}
+} 
+
+
